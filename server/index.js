dcf23f0d3dedae8ef9b7d95ea0c602741816c4a0
jcwright16 <jasoncraigwright@gmail.com>
2025-01-19 23:02:41 -0500

    Now running app with react front end

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server! "})
+    res.json({ message: "Hello from server!" })
 })
 
 app.listen(port, () => {
